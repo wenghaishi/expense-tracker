7cd833248747f1b13cbdbda32d3df8f4678cabde
sam <wenghaishi1@gmail.com>
2023-01-03 13:28:32 +0800

    first commit

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import './ExpenseForm.css'
+
+function ExpenseForm(props) {
+
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+
+        console.log(expenseData)
+        
+        props.onSaveExpenseData();
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+
+    }
+  return (
+    <form onSubmit={submitHandler}>
+    <div className='new-expense__control'>
+        <div className='new-expense__control'>
+            <label>Title</label>
+            <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+        </div>
+        <div className='new-expense__control'>
+            <label>Amount (In numbers) </label>
+            <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
+        </div>
+        <div className='new-expense__control'>
+            <label>Date</label>
+            <input type='date' min="2023-01-01" max="2025-01-01" value={enteredDate} onChange={dateChangeHandler}/>
+        </div>
+        <div className='new-expense__action'>
+            <button type="submit">Add Expense</button>
+        </div>
+    </div>
+    </form>
+  )
+}
+
+export default ExpenseForm
