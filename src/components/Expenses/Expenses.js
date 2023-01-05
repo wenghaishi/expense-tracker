4f0044019e4ded4442e7a93cf1162cb76f4c300b
sam <wenghaishi1@gmail.com>
2023-01-05 16:00:04 +0800

    added default state for filteredyear

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,7 +8,7 @@ import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
 
-  const [filteredYear, setFilteredYear] = useState('');
+  const [filteredYear, setFilteredYear] = useState('2023');
 
   const filterChangeHandler = selectedYear => {
       setFilteredYear(selectedYear)
