34c495492ffa0a11ca4a86b0d38b2478a9eb06c3
sam <wenghaishi1@gmail.com>
2023-01-05 17:34:16 +0800

    added function to edit title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,21 +5,41 @@ import Card from "../../Ui/Card";
 
 function ExpenseItem(props) {
 
-  const [title, setTitle] = useState(props.title);
+  const [title, setTitle] = useState(props.title)
+  const [editTitle, setEditTitle] = useState(false)
+  const [newTitle, setNewTitle] = useState('')
 
-  const clickHandler = () => {
-    setTitle('updated!');
-    console.log(title);
+  const submitHandler = (event) => {
+    event.preventDefault()
+    setTitle(newTitle);
+    setEditTitle(false)
+    console.log(title)
   };
+
+  const editHandler = () => {
+    setEditTitle(true)
+  }
+
+  const titleChangeHandler = (event) => {
+    setNewTitle(event.target.value)
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler} className="change-button">Change title</button>
+      {editTitle === false && <button onClick={editHandler} className="change-button">Change title</button>}
+      {editTitle === true && 
+        <form className="form" onSubmit={submitHandler}>
+          <div className="label-input">
+            <label className="new-title-text">New title</label>
+            <input type="text" className="change-title-input" value={newTitle} onChange={titleChangeHandler}></input>
+          </div>
+          <button type="submit" className="change-button" >Change title</button>
+        </form>}
     </Card>
   );
 }
